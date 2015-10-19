dc5a436f05af5c9ded17fdd6dbd334b6dc0f1934
Jefferson Quesado <jeff.quesado@gmail.com>
2015-10-19 09:05:07 -0300

    Started perceptron and its "and" test case

---
+++ src/main/java/br/com/jeffque/neural/Perceptron.java
@@ -0,0 +1,67 @@
+package br.com.jeffque.neural;
+
+import br.com.jeffque.neural.sample.TrainingSample;
+import br.com.jeffque.neural.sample.TrainingSamplePool;
+import br.com.jeffque.neural.utils.Context;
+import br.com.jeffque.neural.utils.Function;
+import br.com.jeffque.neural.utils.NeuralUtils;
+import br.com.jeffque.neural.utils.TrainingFactorYielder;
+
+public class Perceptron {
+	private double[] weights;
+	private double bias;
+	private Function activator;
+
+	public Perceptron(int inputs, Function activator, Context context) {
+		this(NeuralUtils.randomizedWeigths(inputs, context), NeuralUtils.random(context), activator);
+	}
+	
+	public Perceptron(double[] weights, double bias, Function activator) {
+		this.weights = weights;
+		this.bias = bias;
+		this.activator = activator;
+	}
+	
+	public int getInputSize() {
+		return weights.length;
+	}
+	
+	public double judgeInput(double[] input) {
+		return activator.y(NeuralUtils.dotProduct(input, weights) - bias);
+	}
+	
+	public int training(TrainingFactorYielder trainingFactor, TrainingSamplePool pool, double acceptance) {
+		int age = 0;
+		boolean existsError;
+		
+		if (pool.getSize() == 0) {
+			throw new RuntimeException();
+		}
+		
+		if (pool.getSample(0).getSize() != getInputSize()) {
+			throw new RuntimeException();
+		}
+		
+		do {
+			existsError = false;
+			
+			for (TrainingSample sample: pool) {
+				double[] input = sample.getValues();
+				double y = judgeInput(input);
+				double error = sample.error(y);
+				
+				if (Math.abs(error) > acceptance) {
+					existsError = true;
+					double aleph = trainingFactor.yieldAleph(age);
+					
+					for (int i = weights.length - 1; i >= 0; i--) {
+						weights[i] = weights[i] - aleph *(error) * input[i];
+					}
+				}
+			}
+		} while(existsError);
+		
+		return age;
+	}
+	
+}
---
+++ src/test/java/br/com/jeffque/neural/PerceptronAnd.java
@@ -0,0 +1,24 @@
+package br.com.jeffque.neural;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.com.jeffque.neural.sample.TrainingSamplePool;
+
+public class PerceptronAnd {
+	
+	private static TrainingSamplePool pool;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		pool = new TrainingSamplePool();
+	}
+
+	@Test
+	public void testTraining() {
+		fail("Not yet implemented");
+	}
+
+}
