3fe98db75141b5d824f6e4328ed72a137c2138bf
Jefferson Quesado <jeff.quesado@gmail.com>
2016-02-20 17:14:42 -0300

    Testing for a perceptron that recognizes "AND" operation

--- src/test/java/br/com/jeffque/neural/PerceptronAnd.java
+++ src/test/java/br/com/jeffque/neural/PerceptronAnd.java
@@ -1,34 +1,73 @@
 package br.com.jeffque.neural;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.jeffque.neural.activator.HardLimiter;
+import br.com.jeffque.neural.activator.SymmetricHardLimiter;
 import br.com.jeffque.neural.sample.TrainingSample;
 import br.com.jeffque.neural.sample.TrainingSamplePool;
+import br.com.jeffque.neural.utils.TrainingFactorYielder;
 
 public class PerceptronAnd {
 	
-	private static TrainingSamplePool pool;
+	private static TrainingSamplePool poolSymmetricHardLimit;
+	private static TrainingSamplePool poolHardLimit;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() {
-		pool = new TrainingSamplePool();
+		poolSymmetricHardLimit = new TrainingSamplePool();
+		TrainingSample sampleSTT = new TrainingSample(new double[] { 1.0,  1.0},  1.0);
+		TrainingSample sampleSTF = new TrainingSample(new double[] { 1.0, -1.0}, -1.0);
+		TrainingSample sampleSFF = new TrainingSample(new double[] {-1.0, -1.0}, -1.0);
+		TrainingSample sampleSFT = new TrainingSample(new double[] {-1.0,  1.0}, -1.0);
+		
+		poolSymmetricHardLimit.addSample(sampleSTT);
+		poolSymmetricHardLimit.addSample(sampleSFF);
+		poolSymmetricHardLimit.addSample(sampleSTF);
+		poolSymmetricHardLimit.addValidation(sampleSFT);
+		
+		poolHardLimit = new TrainingSamplePool();
 		TrainingSample sampleTT = new TrainingSample(new double[] {1.0, 1.0}, 1.0);
 		TrainingSample sampleTF = new TrainingSample(new double[] {1.0, 0.0}, 0.0);
 		TrainingSample sampleFF = new TrainingSample(new double[] {0.0, 0.0}, 0.0);
 		TrainingSample sampleFT = new TrainingSample(new double[] {0.0, 1.0}, 0.0);
 		
-		pool.addSample(sampleTT);
-		pool.addSample(sampleFF);
-		pool.addSample(sampleTF);
-		pool.addValidation(sampleFT);
+		poolHardLimit.addSample(sampleTT);
+		poolHardLimit.addSample(sampleFF);
+		poolHardLimit.addSample(sampleTF);
+		poolHardLimit.addValidation(sampleFT);
 	}
-
+	
+	private void testTraining(Perceptron perceptronAnd, TrainingSamplePool pool) {
+		pool.setUseTrainingPool(true);
+		pool.setUseValidationPool(false);
+		@SuppressWarnings("unused")
+		int trainingAge = perceptronAnd.training(new TrainingFactorYielder() {
+			
+			@Override
+			public double yieldAleph(int trainingAge) {
+				return 0.1;
+			}
+		}, pool, 0.01);
+		
+		pool.setUseValidationPool(true);
+		assertThat(perceptronAnd.validation(pool, 0.01), equalTo(0));
+	}
+	
 	@Test
-	public void testTraining() {
-		fail("Not yet implemented");
+	public void testTrainingSymetricHardLimit() {
+		Perceptron perceptronAnd = new Perceptron(new double[] {2.0, 0.2}, 0.25, new SymmetricHardLimiter());
+		testTraining(perceptronAnd, poolSymmetricHardLimit);
+	}
+	
+	@Test
+	public void testTrainingHardLimit() {
+		Perceptron perceptronAnd = new Perceptron(new double[] {2.0, 0.2}, 0.25, new HardLimiter());
+		testTraining(perceptronAnd, poolHardLimit);
 	}
 
 }
