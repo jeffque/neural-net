d8c4ae02cc7553287ff2190032618ad8e3e7be80
Jefferson Quesado <jeff.quesado@gmail.com>
2016-02-20 16:32:49 -0300

    Setting pool up

--- src/test/java/br/com/jeffque/neural/PerceptronAnd.java
+++ src/test/java/br/com/jeffque/neural/PerceptronAnd.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.jeffque.neural.sample.TrainingSample;
 import br.com.jeffque.neural.sample.TrainingSamplePool;
 
 public class PerceptronAnd {
@@ -12,8 +13,17 @@ public class PerceptronAnd {
 	private static TrainingSamplePool pool;
 	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		pool = new TrainingSamplePool();
+		TrainingSample sampleTT = new TrainingSample(new double[] {1.0, 1.0}, 1.0);
+		TrainingSample sampleTF = new TrainingSample(new double[] {1.0, 0.0}, 0.0);
+		TrainingSample sampleFF = new TrainingSample(new double[] {0.0, 0.0}, 0.0);
+		TrainingSample sampleFT = new TrainingSample(new double[] {0.0, 1.0}, 0.0);
+		
+		pool.addSample(sampleTT);
+		pool.addSample(sampleFF);
+		pool.addSample(sampleTF);
+		pool.addValidation(sampleFT);
 	}
 
 	@Test
