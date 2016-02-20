870b8d2ce4d1ffb02a0ea513a069ff3d3cb208a2
Jefferson Quesado <jeff.quesado@gmail.com>
2016-02-20 15:58:09 -0300

    Preparing to use a validation sample

--- src/main/java/br/com/jeffque/neural/sample/TrainingSamplePool.java
+++ src/main/java/br/com/jeffque/neural/sample/TrainingSamplePool.java
@@ -1,21 +1,44 @@
 package br.com.jeffque.neural.sample;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class TrainingSamplePool implements Iterable<TrainingSample> {
-	private List<TrainingSample> pool;
+	private boolean useTrainingPool = true;
 	
-	public TrainingSample getSample(int n) {
-		return pool.get(n);
+	private List<TrainingSample> trainingPool = new ArrayList<>();
+	
+	public void addSample(TrainingSample sample) {
+		trainingPool.add(sample);
+	}
+	
+	public TrainingSample getSample(int idx) {
+		int n = idx;
+		if (useTrainingPool) {
+			try {
+				return trainingPool.get(n);
+			} catch (IndexOutOfBoundsException e) {
+				n -= trainingPool.size();
+			}
+		}
+		throw new IndexOutOfBoundsException("Index " + idx + " is out of the range for: " + (useTrainingPool? " training":""));
 	}
 	
 	public int getSize() {
-		return pool.size();
+		int size = 0;
+		if (useTrainingPool) {
+			size += trainingPool.size();
+		}
+		return size;
 	}
 
 	@Override
 	public Iterator<TrainingSample> iterator() {
-		return pool.iterator();
+		List<TrainingSample> usedSample = new ArrayList<>();
+		if (useTrainingPool) {
+			usedSample.addAll(trainingPool);
+		}
+		return usedSample.iterator();
 	}
 }
